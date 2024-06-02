8b0194a61cf4a1970038906762d8504260ca6eaa
Md. Alif Babu <muhammad.alifbabu@gmail.com>
2024-06-02 18:32:33 +0600

    initial commit

---
+++ src/main/java/com/example/customannotationbegin/CustomAnnotationBeginApplication.java
@@ -0,0 +1,13 @@
+package com.example.customannotationbegin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomAnnotationBeginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomAnnotationBeginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/customannotationbegin/CustomAnnotationBeginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customannotationbegin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomAnnotationBeginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
